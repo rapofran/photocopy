a06b7b55f326fc027e8860ecc6977cfc962ac805
J. Francisco Raposeiras <rapofran@gmail.com>
2013-03-19 15:09:17 -0300

    Omniauth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '470807482986156', '0e7b31492fcf6d2ada78e4692dbb22b0'
   # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Photocopy2::Application.routes.draw do
 
   resources :print_works
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
